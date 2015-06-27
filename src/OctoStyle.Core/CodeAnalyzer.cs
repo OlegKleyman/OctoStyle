aeb33b4680b66762eac76415aae94f6eed90416c
Oleg Kleyman <Omego2K@gmail.com>
2015-06-27 16:56:26 -0400

    add stylecop reference to integration test project, rename class to CodeAnalyzer, add analyze method

---
+++ src/OctoStyle.Core/CodeAnalyzer.cs
@@ -0,0 +1,14 @@
+namespace OctoStyle.Core
+{
+    using System.Collections.Generic;
+
+    using StyleCop;
+
+    public class CodeAnalyzer
+    {
+        public IEnumerable<Violation> Analyze(string projectPath, string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
