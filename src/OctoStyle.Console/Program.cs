ba0ee9df069db0f511d0f8d756af01600ab68fcb
Oleg Kleyman <Omego2K@gmail.com>
2015-06-28 18:59:04 -0400

    implement Arguments parser in the driver method

--- src/OctoStyle.Console/Program.cs
+++ src/OctoStyle.Console/Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace OctoStyle.Console
 {
+    using System;
+
     public class Program
     {
         public static void Main(string[] args)
         {
+            Arguments arguments;
+
+            try
+            {
+                arguments = Arguments.Parse(args);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
         }
     }
 }
