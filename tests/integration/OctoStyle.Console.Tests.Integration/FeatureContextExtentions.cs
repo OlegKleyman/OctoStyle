a34d588d976b786ca2af78828a52a969269f0e0c
Oleg Kleyman <Omego2K@gmail.com>
2015-06-28 11:54:53 -0400

    added repository owner property

--- tests/integration/OctoStyle.Console.Tests.Integration/FeatureContextExtentions.cs
+++ tests/integration/OctoStyle.Console.Tests.Integration/FeatureContextExtentions.cs
@@ -1,15 +1,42 @@
 ﻿namespace OctoStyle.Console.Tests.Integration
 {
+    using System;
+    using System.Collections.Generic;
+
     using TechTalk.SpecFlow;
 
     public class FeatureContextExtended : FeatureContext
     {
+        private const string RepositoryOwnerKey = "REPOSITORY_OWNER";
+
         private readonly FeatureContext currentContext;
 
         public FeatureContextExtended(FeatureContext context)
             : base(context.FeatureInfo, context.BindingCulture)
         {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
             this.currentContext = context;
+        }
+
+        public string RepositoryOwner
+        {
+            get
+            {
+                if (!this.currentContext.ContainsKey(RepositoryOwnerKey))
+                {
+                    throw new KeyNotFoundException(RepositoryOwnerKey);
+                }
+
+                return this.currentContext.Get<string>(RepositoryOwnerKey);
+            }
+            set
+            {
+                this.currentContext.Set(value, RepositoryOwnerKey);
+            }
         }
     }
 }
