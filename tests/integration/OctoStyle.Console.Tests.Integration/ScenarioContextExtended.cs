3861f7b3ca04f2cfa09644733c8c28502808decb
Oleg Kleyman <Omego2K@gmail.com>
2015-06-28 12:28:48 -0400

    add ScenarioContextExtended

---
+++ tests/integration/OctoStyle.Console.Tests.Integration/ScenarioContextExtended.cs
@@ -0,0 +1,21 @@
+﻿namespace OctoStyle.Console.Tests.Integration
+{
+    using System;
+
+    using TechTalk.SpecFlow;
+
+    public class ScenarioContextExtended
+    {
+        public ScenarioContext Context { get; set; }
+
+        public ScenarioContextExtended(ScenarioContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            this.Context = context;
+        }
+    }
+}
