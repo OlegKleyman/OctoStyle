d4b5ddaadf8b6ea17dbff1cb6e093b32744e1d9d
Oleg Kleyman <Omego2K@gmail.com>
2015-06-28 12:46:37 -0400

    add test solution

---
+++ common/OctoStyleTest/src/TestLibrary/Nested/TestClass2.cs
@@ -0,0 +1,13 @@
+﻿namespace TestLibrary
+{
+    public class TestClass2
+    {
+        public void testMethod()
+        {
+            foreach (var i in new [] {1,2,3,4})
+            {
+                var TestVar = 3;
+            }
+        }
+    }
+}
