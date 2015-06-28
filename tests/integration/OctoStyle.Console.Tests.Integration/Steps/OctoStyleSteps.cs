1f30bf97595e2a19082e77cd9793c788cd206cbc
Oleg Kleyman <Omego2K@gmail.com>
2015-06-28 11:31:09 -0400

    add step methods

--- tests/integration/OctoStyle.Console.Tests.Integration/Steps/OctoStyleSteps.cs
+++ tests/integration/OctoStyle.Console.Tests.Integration/Steps/OctoStyleSteps.cs
@@ -5,5 +5,22 @@
     [Binding]
     public class OctoStyleSteps : Steps
     {
+        [Given(@"I have a pull request with stylistic problems")]
+        public void GivenIHaveAPullRequestWithStylisticProblems()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I run the OctoStyle")]
+        public void WhenIRunTheOctoStyle()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"there should be comments on the pull request on the lines of the found violations")]
+        public void ThenThereShouldBeCommentsOnThePullRequestOnTheLinesOfTheFoundViolations()
+        {
+            ScenarioContext.Current.Pending();
+        }
     }
 }
