9ff1c95e8954aeba7fb2c180ad3e9273563bc3c8
Oleg Kleyman <Omego2K@gmail.com>
2015-06-28 15:53:28 -0400

    implement step to assert that comments have been created

--- tests/integration/OctoStyle.Console.Tests.Integration/FeatureSteps/OctoStyleSteps.cs
+++ tests/integration/OctoStyle.Console.Tests.Integration/FeatureSteps/OctoStyleSteps.cs
@@ -2,6 +2,11 @@
 {
     using System.Globalization;
     using System.IO;
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    using Octokit;
 
     using TechTalk.SpecFlow;
 
@@ -13,6 +18,7 @@
         {
             FeatureContextExtended.Current.RepositoryOwner = "OlegKleyman";
             FeatureContextExtended.Current.Repository = "OctoStyleTest";
+            FeatureContextExtended.Current.GitClient = new GitHubClient(new ProductHeaderValue("IntegrationTests"));
         }
 
         [Given(@"I have a pull request with stylistic problems")]
@@ -34,7 +40,43 @@
         [Then(@"there should be comments on the pull request on the lines of the found violations")]
         public void ThenThereShouldBeCommentsOnThePullRequestOnTheLinesOfTheFoundViolations()
         {
-            ScenarioContext.Current.Pending();
+            var client = FeatureContextExtended.Current.GitClient;
+
+            var comments = client.PullRequest.Comment.GetAll(
+                FeatureContextExtended.Current.RepositoryOwner,
+                FeatureContextExtended.Current.Repository,
+                ScenarioContextExtended.Current.PullRequestNumber).GetAwaiter().GetResult();
+
+            var testClassComments =
+                comments.Where(
+                    comment => (comment.Path.EndsWith("TestClass.cs") && comment.Position > 3 && comment.Position < 12))
+                    .ToList();
+
+            var testClass2Comments =
+                comments.Where(comment => (comment.Path.EndsWith("TestClass2.cs") && comment.Position == 9)).ToList();
+
+            Assert.That(testClassComments.Count, Is.GreaterThanOrEqualTo(2));
+            
+            Assert.That(
+                testClassComments.Any(
+                    comment =>
+                    comment.Body == "SA1600: The method must have a documentation header." && comment.Position == 5));
+
+            Assert.That(
+                testClassComments.Any(
+                    comment =>
+                    comment.Body
+                    == "SA1513: Statements or elements wrapped in curly brackets must be followed by a blank line."
+                    && comment.Position == 9));
+
+            Assert.That(testClass2Comments.Count, Is.GreaterThanOrEqualTo(1));
+
+            Assert.That(
+                testClass2Comments.Any(
+                    comment =>
+                    comment.Body
+                    == "SA1306: Variable names and private field names must start with a lower-case letter: TestVar."
+                    && comment.Position == 9));
         }
     }
 }
