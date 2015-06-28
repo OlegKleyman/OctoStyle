432c9baebae7608bd44aa6e79701df8d64ad9a4f
Oleg Kleyman <Omego2K@gmail.com>
2015-06-28 12:38:50 -0400

    add pull reuest property

--- tests/integration/OctoStyle.Console.Tests.Integration/ScenarioContextExtended.cs
+++ tests/integration/OctoStyle.Console.Tests.Integration/ScenarioContextExtended.cs
@@ -1,11 +1,14 @@
 ﻿namespace OctoStyle.Console.Tests.Integration
 {
     using System;
+    using System.Collections.Generic;
 
     using TechTalk.SpecFlow;
 
     public class ScenarioContextExtended
     {
+        private const string PullRequestNumberKey = "PULL_REQUEST_NUMBER";
+
         private static readonly ScenarioContextExtended CurrentContext = new ScenarioContextExtended(ScenarioContext.Current);
 
         public static ScenarioContextExtended Current
@@ -26,6 +29,23 @@
             }
 
             this.context = context;
+        }
+
+        public int PullRequestNumber
+        {
+            get
+            {
+                if (!this.context.ContainsKey(PullRequestNumberKey))
+                {
+                    throw new KeyNotFoundException(PullRequestNumberKey);
+                }
+
+                return this.context.Get<int>(PullRequestNumberKey);
+            }
+            set
+            {
+                this.context.Set(value, PullRequestNumberKey);
+            }
         }
     }
 }
