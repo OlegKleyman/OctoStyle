3f94a3eea8a04fdc98a13b9f0b9be8c5f2cfdbe0
Oleg Kleyman <Omego2K@gmail.com>
2015-07-12 09:30:46 -0400

    add factory class for retrieving git diff entry

---
+++ src/OctoStyle.Core/GitDiffEntryFactory.cs
@@ -0,0 +1,40 @@
+﻿namespace OctoStyle.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    using OctoStyle.Core.Borrowed;
+
+    public class GitDiffEntryFactory
+    {
+        public IReadOnlyList<GitDiffEntry> Get(DiffEntry<string> entry, int position)
+        {
+            var gitDiff = new List<GitDiffEntry>();
+
+            switch (entry.EntryType)
+            {
+                case DiffEntryType.Equal:
+                    for (var i = 0; i < entry.Count; i++)
+                    {
+                        gitDiff.Add(new EqualGitDiffEntry(position++));
+                    }
+                    break;
+                case DiffEntryType.Add:
+                    gitDiff.Add(new ModificationGitDiffEntry(position++, GitDiffEntryStatus.New, entry.LineNumber));
+                    break;
+                case DiffEntryType.Remove:
+                    gitDiff.Add(new ModificationGitDiffEntry(position++, GitDiffEntryStatus.Removed, entry.LineNumber));
+                    break;
+                default:
+                    throw new InvalidOperationException(
+                        String.Format(
+                            CultureInfo.InvariantCulture,
+                            "Unable to convert DiffEntry of type {0} to GitDiff",
+                            entry.EntryType));
+            }
+
+            return gitDiff;
+        }
+    }
+}
