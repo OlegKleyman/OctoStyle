73dab4c62b18fde945ac5cabcc22041ba93a22cb
Oleg Kleyman <Omego2K@gmail.com>
2015-07-12 10:19:34 -0400

    add scaffolding for actions to take pending file status

--- src/OctoStyle.Console/Program.cs
+++ src/OctoStyle.Console/Program.cs
@@ -22,20 +22,36 @@
             }
 
             var client = new GitHubClient(new ProductHeaderValue("OctoStyle"));
-            
-            var pullRequestUrlFormat = String.Format(
-                CultureInfo.InvariantCulture,
-                "https://api.github.com/repos/{0}/{1}/pulls/{2}",
-                arguments.RepositoryOwner,
-                arguments.Repository,
-                arguments.PullRequestNumber);
-            
-            var rawDiff =
-                client.Connection.Get<string>(
-                    new Uri(pullRequestUrlFormat),
-                    null,
-                    "application/vnd.github.VERSION.diff",
-                    true).GetAwaiter().GetResult();
+
+            var files =
+                client.PullRequest.Files(arguments.RepositoryOwner, arguments.Repository, arguments.PullRequestNumber)
+                    .GetAwaiter()
+                    .GetResult();
+
+            foreach (var file in files)
+            {
+                if (file.Status == "modified")
+                {
+                    
+                }
+                else if (file.Status == "added")
+                {
+                    
+                }
+                else if (file.Status == "renamed")
+                {
+                    
+                }
+                else if (file.Status == "deleted")
+                {
+                    //no work to do
+                }
+                else
+                {
+                    throw new InvalidOperationException(
+                        String.Format(CultureInfo.InvariantCulture, "Unknown file status: {0}.", file.Status));
+                }
+            }
         }
     }
 }
