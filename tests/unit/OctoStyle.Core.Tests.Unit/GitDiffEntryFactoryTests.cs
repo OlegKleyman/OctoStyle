93177d9fbc59b1c70b4d92a6878ebd9059594c9d
Oleg Kleyman <Omego2K@gmail.com>
2015-07-12 09:31:15 -0400

    add test method for equal diff

--- tests/unit/OctoStyle.Core.Tests.Unit/GitDiffEntryFactoryTests.cs
+++ tests/unit/OctoStyle.Core.Tests.Unit/GitDiffEntryFactoryTests.cs
@@ -2,8 +2,35 @@
 {
     using NUnit.Framework;
 
+    using OctoStyle.Core.Borrowed;
+
     [TestFixture]
     public class GitDiffEntryFactoryTests
     {
+        [Test]
+        public void GetEntryShouldReturnEqualGitDiffEntry()
+        {
+            var factory = GetGitDiffEntryFactory();
+
+            var entry = new DiffEntry<string>(DiffEntryType.Equal, null, 0);
+            entry.Count = 3;
+
+            var result = factory.Get(entry, 3);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count, Is.EqualTo(3));
+            
+            Assert.That(result[0], Is.InstanceOf<EqualGitDiffEntry>());
+            Assert.That(result[0].Position, Is.EqualTo(3));
+            Assert.That(result[1], Is.InstanceOf<EqualGitDiffEntry>());
+            Assert.That(result[1].Position, Is.EqualTo(4));
+            Assert.That(result[2], Is.InstanceOf<EqualGitDiffEntry>());
+            Assert.That(result[2].Position, Is.EqualTo(5));
+        }
+
+        private GitDiffEntryFactory GetGitDiffEntryFactory()
+        {
+            return new GitDiffEntryFactory();
+        }
     }
 }
