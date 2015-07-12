723bfb8b17419b1a3d301c0c645111af173e3d98
Oleg Kleyman <Omego2K@gmail.com>
2015-07-12 12:09:36 -0400

    comment with not supported on move files if changes exist

--- src/OctoStyle.Console/Program.cs
+++ src/OctoStyle.Console/Program.cs
@@ -2,8 +2,10 @@
 {
     using System;
     using System.Globalization;
+    using System.Linq;
 
     using Octokit;
+    using Octokit.Internal;
 
     public class Program
     {
@@ -21,7 +23,41 @@
                 return;
             }
 
-            var client = new GitHubClient(new ProductHeaderValue("OctoStyle"));
+            var client = new GitHubClient(
+                new ProductHeaderValue("OctoStyle"),
+                new InMemoryCredentialStore(
+                    new Credentials(arguments.Login, "e5459d51eed64fd5e9f7ae947ca444d621aa386a")));
+
+            var pullrequestInformationMessage = String.Format(
+                CultureInfo.InvariantCulture,
+                "{0}Owner: {1}, Repository: {2}, Number: {3}",
+                Environment.NewLine,
+                arguments.RepositoryOwner,
+                arguments.Repository,
+                arguments.PullRequestNumber);
+
+            var commits =
+                client.PullRequest.Commits(arguments.RepositoryOwner, arguments.Repository, arguments.PullRequestNumber)
+                    .GetAwaiter()
+                    .GetResult();
+
+            if (commits == null)
+            {
+                throw new InvalidOperationException(
+                    String.Format(
+                        CultureInfo.InvariantCulture,
+                        "Unable to retrieve commits for pull request{0}",
+                        pullrequestInformationMessage));
+            }
+
+            if (commits.Count == 0)
+            {
+                throw new InvalidOperationException(
+                    String.Format(
+                        CultureInfo.InvariantCulture,
+                        "No commits found for pull request{0}",
+                        pullrequestInformationMessage));
+            }
 
             var files =
                 client.PullRequest.Files(arguments.RepositoryOwner, arguments.Repository, arguments.PullRequestNumber)
@@ -40,7 +76,16 @@
                 }
                 else if (file.Status == "renamed")
                 {
-                    
+                    if (file.Changes > 0)
+                    {
+                        var comment = new PullRequestReviewCommentCreate("Renamed files not supported", commits.Last().Sha, file.FileName, 1);
+
+                        client.PullRequest.Comment.Create(
+                            arguments.RepositoryOwner,
+                            arguments.Repository,
+                            arguments.PullRequestNumber,
+                            comment).GetAwaiter().GetResult();
+                    }
                 }
                 else if (file.Status == "deleted")
                 {
