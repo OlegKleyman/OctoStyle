670c716001b646515c3ef3876c71239574d8829a
Oleg Kleyman <Omego2K@gmail.com>
2015-07-18 10:29:24 -0400

    add FileSystemManager

---
+++ src/OctoStyle.Core/FileSystemManager.cs
@@ -0,0 +1,21 @@
+﻿namespace OctoStyle.Core
+{
+    using System;
+    using System.IO;
+
+    public class FileSystemManager : IFileSystemManager
+    {
+        /// <summary>
+        ///     Gets files from a directory matching a filter.
+        /// </summary>
+        /// <param name="targetDirectory"> The directory to investigate files in. </param>
+        /// <param name="fileFilter"> The filter of the files to be retrieved. </param>
+        /// <returns>
+        ///     A <see cref="System.String"/> array of file paths.
+        /// </returns>
+        public string[] GetFiles(string targetDirectory, string fileFilter)
+        {
+            return Directory.GetFiles(targetDirectory, fileFilter);
+        }
+    }
+}
---
+++ src/OctoStyle.Core/IFileSystemManager.cs
@@ -0,0 +1,15 @@
+﻿namespace OctoStyle.Core
+{
+    public interface IFileSystemManager
+    {
+        /// <summary>
+        ///     Gets files from a directory matching a filter.
+        /// </summary>
+        /// <param name="targetDirectory"> The directory to investigate files in. </param>
+        /// <param name="fileFilter"> The filter of the files to be retrieved. </param>
+        /// <returns>
+        ///     A <see cref="System.String"/> array of file paths.
+        /// </returns>
+        string[] GetFiles(string targetDirectory, string fileFilter);
+    }
+}
