62ef6d865e9f3b04ecbe7008f4e1eba3c00b7b30
Oleg Kleyman <Omego2K@gmail.com>
2015-07-18 10:28:58 -0400

    complete FileSystemManager Test

--- tests/integration/OctoStyle.Core.Tests.Integration/FileSystemManagerTests.cs
+++ tests/integration/OctoStyle.Core.Tests.Integration/FileSystemManagerTests.cs
@@ -5,5 +5,41 @@
     [TestFixture]
     public class FileSystemManagerTests
     {
+        private static readonly object[] GetFilesShouldReturnFilesInDirectoryCases =
+            {
+                new object[]{ @"FileSystemManagerFiles", "*.txt", new[]
+                                                                      {
+                                                                          @"FileSystemManagerFiles\TestFile1.txt",
+                                                                          @"FileSystemManagerFiles\TestFile2.txt",
+                                                                          @"FileSystemManagerFiles\TestFile3.txt",
+                                                                          @"FileSystemManagerFiles\TestFile4.txt",
+                                                                      } },
+                new object[]{ @"FileSystemManagerFiles", "*.csv", new []
+                                                                      {
+                                                                          
+                                                                          @"FileSystemManagerFiles\TestFile1.csv",
+                                                                          @"FileSystemManagerFiles\TestFile2.csv",
+                                                                      } }
+            };
+
+        [Test]
+        [TestCaseSource("GetFilesShouldReturnFilesInDirectoryCases")]
+        public void GetFilesShouldReturnFilesInDirectory(string directoryPath, string fileFilter, string[] expectedFiles)
+        {
+            var manager = GetFileSystemManager();
+            var files = manager.GetFiles(directoryPath, fileFilter);
+
+            Assert.That(files.Length, Is.EqualTo(expectedFiles.Length));
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                Assert.That(files[i], Is.EqualTo(expectedFiles[i]));
+            }
+        }
+
+        private FileSystemManager GetFileSystemManager()
+        {
+            return new FileSystemManager();
+        }
     }
 }
