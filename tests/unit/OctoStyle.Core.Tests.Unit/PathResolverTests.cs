52bba416cf49c04f98222468ebcd32b860428a28
Oleg Kleyman <Omego2K@gmail.com>
2015-07-18 10:56:44 -0400

    complete GetPath test

--- tests/unit/OctoStyle.Core.Tests.Unit/PathResolverTests.cs
+++ tests/unit/OctoStyle.Core.Tests.Unit/PathResolverTests.cs
@@ -1,24 +1,48 @@
 ﻿namespace OctoStyle.Core.Tests.Unit
 {
+    using System;
+
+    using Moq;
+
     using NUnit.Framework;
 
     [TestFixture]
     public class PathResolverTests
     {
-        [TestCase(@"C:\testPath\innerDirectory1\innerDirectory2\someFile.cs", @"C:\testPath")]
-        [TestCase(@"C:\testPath\innerDirectory1\innerDirectory2", @"C:\testPath")]
-        [TestCase(@"C:\testPath\innerDirectory1", @"C:\testPath")]
-        [TestCase(@"C:\testPath", @"C:\testPath")]
+        private string ProjectFileFilter = "*.csproj";
+        private const string GetPathShouldReturnPathInitialFilePath = @"C:\testPath\innerDirectory1\innerDirectory2\someFile.cs";
+        private const string GetPathShouldReturnPathInitialOuterMostDirectoryPath = @"C:\testPath\innerDirectory1";
+        private const string GetPathShouldReturnPathInitialMiddleDirectoryPath = @"C:\testPath\innerDirectory1";
+        private const string ProjectPath = @"C:\testPath";
+
+        [TestCase(GetPathShouldReturnPathInitialFilePath, ProjectPath)]
+        [TestCase(GetPathShouldReturnPathInitialOuterMostDirectoryPath, ProjectPath)]
+        [TestCase(GetPathShouldReturnPathInitialMiddleDirectoryPath, ProjectPath)]
+        [TestCase(ProjectPath, ProjectPath)]
         public void GetPathShouldReturnPath(string initialPath, string expectedPath)
         {
             IPathResolver resolver = GetPathResolver();
-            var path = resolver.GetPath(initialPath, "*.csproj");
+            var path = resolver.GetPath(initialPath, this.ProjectFileFilter);
             Assert.That(path, Is.EqualTo(expectedPath));
         }
 
         private PathResolver GetPathResolver()
         {
-            return new PathResolver();
+            var mockFileSystemManager = new Mock<IFileSystemManager>();
+
+            mockFileSystemManager.Setup(
+                manager => manager.GetFiles(GetPathShouldReturnPathInitialFilePath, this.ProjectFileFilter))
+                .Returns(new string[] { });
+            mockFileSystemManager.Setup(
+                manager => manager.GetFiles(GetPathShouldReturnPathInitialOuterMostDirectoryPath, this.ProjectFileFilter))
+                .Returns(new string[] { });
+            mockFileSystemManager.Setup(
+                manager => manager.GetFiles(GetPathShouldReturnPathInitialMiddleDirectoryPath, this.ProjectFileFilter))
+                .Returns(new string[] { });
+            mockFileSystemManager.Setup(manager => manager.GetFiles(ProjectPath, this.ProjectFileFilter))
+                .Returns(new[] { "test.csproj" });
+
+            return new PathResolver(mockFileSystemManager.Object);
         }
     }
 }
