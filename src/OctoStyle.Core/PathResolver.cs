7ad697d87089255e2716cbbb54c18832df3aaed0
Oleg Kleyman <Omego2K@gmail.com>
2015-07-18 09:50:12 -0400

    ad implementation of GetPath

--- src/OctoStyle.Core/PathResolver.cs
+++ src/OctoStyle.Core/PathResolver.cs
@@ -1,10 +1,50 @@
 ﻿namespace OctoStyle.Core
 {
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+
     public class PathResolver : IPathResolver
     {
         public string GetPath(string initialPath, string fileFilter)
         {
-            throw new System.NotImplementedException();
+            if (initialPath == null)
+            {
+                throw new ArgumentNullException("initialPath");
+            }
+
+            if (fileFilter == null)
+            {
+                throw new ArgumentNullException("fileFilter");
+            }
+
+            const string cannotBeEmptyMessage = "Cannot be empty";
+
+            if (initialPath.Length == 0)
+            {
+                throw new ArgumentException(cannotBeEmptyMessage, "initialPath");
+            }
+
+            if (fileFilter.Length == 0)
+            {
+                throw new ArgumentException(cannotBeEmptyMessage, "fileFilter");
+            }
+
+            var directoryName = Path.GetDirectoryName(initialPath);
+            
+            if (directoryName == null)
+            {
+                throw new InvalidOperationException(
+                    String.Format(CultureInfo.InvariantCulture, "Directory name of {0} was not found", initialPath));
+            }
+
+            if (Directory.GetFiles(directoryName, fileFilter).Any())
+            {
+                return directoryName;
+            }
+
+            return GetPath(directoryName, fileFilter);
         }
     }
 }
