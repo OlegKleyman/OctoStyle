730a3f0e425ace91766bd6ffcc9964ba1735d828
Oleg Kleyman <Omego2K@gmail.com>
2015-07-19 18:38:32 -0400

    add NoCommentPullRequestCommenter

--- src/OctoStyle.Core/PullRequestCommenter.cs
+++ src/OctoStyle.Core/PullRequestCommenter.cs
@@ -14,6 +14,8 @@ namespace OctoStyle.Core
 
         private readonly GitRepository repository;
 
+        private PullRequestCommenter() { }
+
         protected PullRequestCommenter(IPullRequestReviewCommentsClient client, GitRepository repository)
         {
             if (client == null)
@@ -29,12 +31,36 @@ namespace OctoStyle.Core
             this.client = client;
             this.repository = repository;
         }
-
+        
         public abstract Task<IEnumerable<PullRequestReviewComment>> Create(GitHubPullRequestFile file, ICodeAnalyzer analyzer, string physicalFilePath);
 
         protected async Task<PullRequestReviewComment> Create(PullRequestReviewCommentCreate comment, int pullRequestNumber)
         {
             return await client.Create(repository.Owner, repository.Name, pullRequestNumber, comment);
+        }
+
+        public class NoCommentPullRequestCommenter : PullRequestCommenter
+        {
+            private NoCommentPullRequestCommenter()
+            {
+            }
+
+            private static readonly NoCommentPullRequestCommenter commenter = new NoCommentPullRequestCommenter();
+
+            public static PullRequestCommenter NoComment
+            {
+                get
+                {
+                    return commenter;
+                }
+            }
+
+            public override Task<IEnumerable<PullRequestReviewComment>> Create(GitHubPullRequestFile file, ICodeAnalyzer analyzer, string physicalFilePath)
+            {
+                var task = new Task<IEnumerable<PullRequestReviewComment>>(() => new List<PullRequestReviewComment>());
+                task.RunSynchronously();
+                return task;
+            }
         }
     }
 }
--- tests/unit/OctoStyle.Core.Tests.Unit/NoCommentPullRequestCommenterTests.cs
+++ tests/unit/OctoStyle.Core.Tests.Unit/NoCommentPullRequestCommenterTests.cs
@@ -1,9 +1,25 @@
 ﻿namespace OctoStyle.Core.Tests.Unit
 {
+    using System.Linq;
+
     using NUnit.Framework;
 
     [TestFixture]
     public class NoCommentPullRequestCommenterTests
     {
+        [Test]
+        public async void CreateShouldReturnEmptyPullRequestComment()
+        {
+            var commenter = GetNoCommentPullRequestCommenter();
+            var comments = await commenter.Create(null, null, null);
+
+            Assert.That(comments, Is.Not.Null);
+            Assert.That(comments.Any(), Is.False);
+        }
+
+        private static PullRequestCommenter GetNoCommentPullRequestCommenter()
+        {
+            return PullRequestCommenter.NoCommentPullRequestCommenter.NoComment;
+        }
     }
 }
