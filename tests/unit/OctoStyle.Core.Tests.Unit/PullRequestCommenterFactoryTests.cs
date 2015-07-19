810984d20ec1d2e3b8b1cef457651a223b385b43
Oleg Kleyman <Omego2K@gmail.com>
2015-07-19 19:07:26 -0400

    complete test fixture

--- tests/unit/OctoStyle.Core.Tests.Unit/PullRequestCommenterFactoryTests.cs
+++ tests/unit/OctoStyle.Core.Tests.Unit/PullRequestCommenterFactoryTests.cs
@@ -1,9 +1,35 @@
 ﻿namespace OctoStyle.Core.Tests.Unit
 {
+    using System;
+
+    using Moq;
+
     using NUnit.Framework;
+
+    using Octokit;
 
     [TestFixture]
     public class PullRequestCommenterFactoryTests
     {
+        [TestCase(GitPullRequestFileStatus.Modified, typeof(ModifiedPullRequestCommenter))]
+        [TestCase(GitPullRequestFileStatus.Deleted, typeof(PullRequestCommenter.NoCommentPullRequestCommenter))]
+        [TestCase(GitPullRequestFileStatus.Added, typeof(AddedPullRequestCommenter))]
+        [TestCase(GitPullRequestFileStatus.Renamed, typeof(RenamedPullRequestCommenter))]
+        public void GetShouldReturnPullRequestCommenterObject(GitPullRequestFileStatus status, Type expectedType)
+        {
+            var factory = GetPullRequestCommenterFactory();
+
+            var commenter = factory.Get(status);
+
+            Assert.That(commenter, Is.InstanceOf(expectedType));
+        }
+
+        private static IPullRequestCommenterFactory GetPullRequestCommenterFactory()
+        {
+            var commentsClient = new Mock<IPullRequestReviewCommentsClient>();
+            var diffRetriever = new Mock<IGitHubDiffRetriever>();
+
+            return new PullRequestCommenterFactory(commentsClient.Object, new GitRepository("OlegKleyman", "OctoStyleTest"), diffRetriever.Object);
+        }
     }
 }
