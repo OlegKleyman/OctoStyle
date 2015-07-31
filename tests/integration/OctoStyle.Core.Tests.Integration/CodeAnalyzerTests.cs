adb61a713fd2cb37b96ffd2a20b39e3fdfb11cd8
Oleg Kleyman <Omego2K@gmail.com>
2015-07-30 20:02:23 -0400

    correct codeanalyzer test and make test stylecop file not merge

--- tests/integration/OctoStyle.Core.Tests.Integration/CodeAnalyzerTests.cs
+++ tests/integration/OctoStyle.Core.Tests.Integration/CodeAnalyzerTests.cs
@@ -17,7 +17,6 @@
         {
             const string relativeSolutionPath = @"..\..";
             var projectPath = Path.GetFullPath(relativeSolutionPath);
-            Console.WriteLine(projectPath);
 
             if (projectPath == null)
             {
@@ -46,9 +45,29 @@
             Assert.That(violations[2].RuleId, Is.EqualTo("SA1513"));
             Assert.That(
                 violations[3].Message,
+                Is.EqualTo("All using directives must be placed inside of the namespace."));
+            Assert.That(violations[3].RuleId, Is.EqualTo("SA1200"));
+            Assert.That(
+                violations[4].Message,
+                Is.EqualTo("All using directives must be placed inside of the namespace."));
+            Assert.That(violations[4].RuleId, Is.EqualTo("SA1200"));
+            Assert.That(
+                violations[5].Message,
+                Is.EqualTo("All using directives must be placed inside of the namespace."));
+            Assert.That(violations[5].RuleId, Is.EqualTo("SA1200"));
+            Assert.That(
+                violations[6].Message,
+                Is.EqualTo("All using directives must be placed inside of the namespace."));
+            Assert.That(violations[6].RuleId, Is.EqualTo("SA1200"));
+            Assert.That(
+                violations[7].Message,
+                Is.EqualTo("All using directives must be placed inside of the namespace."));
+            Assert.That(violations[7].RuleId, Is.EqualTo("SA1200"));
+            Assert.That(
+                violations[8].Message,
                 Is.EqualTo(
                     "The comment must start with a single space. To ignore this error when commenting out a line of code, begin the comment with '////' rather than '//'."));
-            Assert.That(violations[3].RuleId, Is.EqualTo("SA1005"));
+            Assert.That(violations[8].RuleId, Is.EqualTo("SA1005"));
         }
     }
 }
