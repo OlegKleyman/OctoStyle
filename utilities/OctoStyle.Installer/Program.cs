8dff271162e9e63e1ca6c9abf8b52a7c47554bcf
Oleg Kleyman <Omego2K@gmail.com>
2015-08-09 21:06:32 -0400

    add installer code

--- utilities/OctoStyle.Installer/Program.cs
+++ utilities/OctoStyle.Installer/Program.cs
@@ -1,9 +1,37 @@
 ﻿namespace OctoStyle.Installer
 {
+    using System.IO;
+
+    using WixSharp;
+
+    using File = WixSharp.File;
+
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
+            const string mode = 
+#if DEBUG
+            "Debug";
+#else
+            "Release";
+#endif
+
+            var octoStyleOutputPath = Path.Combine(@"..\..\src\OctoStyle.Console\bin\", mode);
+
+            var project =
+            new Project("OctoStyle",
+                new Dir(@"%ProgramFiles%\Omego2K\OctoStyle",
+                    new File(Path.Combine(octoStyleOutputPath, "OctoStyle.Console.exe")),
+                    new File(Path.Combine(octoStyleOutputPath, "OctoStyle.Core.dll")),
+                    new File(Path.Combine(octoStyleOutputPath, "NDesk.Options.dll")),
+                    new File(Path.Combine(octoStyleOutputPath, "Octokit.dll")),
+                    new File(Path.Combine(octoStyleOutputPath, "StyleCop.CSharp.Rules.dll")),
+                    new File(Path.Combine(octoStyleOutputPath, "StyleCop.dll")),
+                    new File(Path.Combine(octoStyleOutputPath, "StyleCop.CSharp.dll"))));
+
+            Compiler.WixLocation = @"..\..\packages\WiX.3.9.2\tools";
+            Compiler.BuildMsi(project);
         }
     }
 }
