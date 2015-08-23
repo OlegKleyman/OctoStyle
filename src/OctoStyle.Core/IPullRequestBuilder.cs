1fc750aa8fc5f5c73296143d2b161132acf11d30
Oleg Kleyman <Omego2K@gmail.com>
2015-08-23 08:43:31 -0400

    implement PullRequestBuilder

--- src/OctoStyle.Core/IPullRequestBuilder.cs
+++ src/OctoStyle.Core/IPullRequestBuilder.cs
@@ -1,6 +1,16 @@
 namespace OctoStyle.Core
 {
+    using System.Collections.Generic;
+
+    using Octokit;
+
     public interface IPullRequestBuilder
     {
+        GitHubPullRequest Build(
+            int number,
+            string lastCommitId,
+            IReadOnlyList<PullRequestFile> files,
+            string diff,
+            GitHubPullRequestBranches branches);
     }
 }
--- src/OctoStyle.Core/PullRequestBuilder.cs
+++ src/OctoStyle.Core/PullRequestBuilder.cs
@@ -1,6 +1,51 @@
 namespace OctoStyle.Core
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Octokit;
+
     public class PullRequestBuilder : IPullRequestBuilder
     {
+        private readonly IDiffParser parser;
+
+        public PullRequestBuilder(IDiffParser parser)
+        {
+            if (parser == null)
+            {
+                throw new ArgumentNullException("parser");
+            }
+
+            this.parser = parser;
+        }
+
+        public GitHubPullRequest Build(
+            int number,
+            string lastCommitId,
+            IReadOnlyList<PullRequestFile> files,
+            string diff,
+            GitHubPullRequestBranches branches)
+        {
+            if (files == null)
+            {
+                throw new ArgumentNullException("files");
+            }
+
+            var diffs = this.parser.Split(diff);
+
+            var pullRequestFiles =
+                files.Select(
+                    file =>
+                    new GitHubPullRequestFile(
+                        file.FileName,
+                        (GitHubPullRequestFileStatus)Enum.Parse(typeof(GitHubPullRequestFileStatus), file.Status, true),
+                        file.Changes,
+                        diffs[file.FileName])).ToList();
+
+            var pullRequest = new GitHubPullRequest(number, lastCommitId, pullRequestFiles, diff, branches);
+
+            return pullRequest;
+        }
     }
 }
