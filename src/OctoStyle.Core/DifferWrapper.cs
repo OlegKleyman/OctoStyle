186434baccdfb028d61f0974deeb54e74a322966
Oleg Kleyman <Omego2K@gmail.com>
2015-08-29 17:24:56 -0400

    add XML documentation to the DifferWrapper class

--- src/OctoStyle.Core/DifferWrapper.cs
+++ src/OctoStyle.Core/DifferWrapper.cs
@@ -5,8 +5,19 @@
     using SharpDiff;
     using SharpDiff.FileStructure;
 
+    /// <summary>
+    /// Represents a wrapper for the <see cref="Differ"/> class.
+    /// </summary>
     public class DifferWrapper : IDiffer
     {
+        /// <summary>
+        /// A wrapper for the <see cref="Differ.Load"/> method.
+        /// </summary>
+        /// <param name="diff">The diff string to load.</param>
+        /// <returns>A <see cref="IEnumerable{T}"/> of <see cref="Diff"/> representation of the diff.</returns>
+        /// <remarks>
+        /// This method will attach new line character at the end of the <paramref name="diff"/> parameter value.
+        /// </remarks>
         public IEnumerable<Diff> Load(string diff)
         {
             return Differ.Load(string.Concat(diff, '\n'));
