d59f16ab5ec23a939a680eab955d958631ec668b
Oleg Kleyman <Omego2K@gmail.com>
2015-09-03 13:11:46 -0400

    remove unused using and variable

--- src/OctoStyle.Console/Program.cs
+++ src/OctoStyle.Console/Program.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Globalization;
     using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
@@ -62,8 +61,6 @@
 
             var builder = new PullRequestBuilder(new DiffParser());
             var pullRequestRetriever = new PullRequestRetriever(builder, client.PullRequest, client.Connection, repository);
-            var con = new Connection(new ProductHeaderValue("OctoStyle"),
-                new InMemoryCredentialStore(new Credentials(arguments.Login, arguments.Password)));
             
             var pullRequest = pullRequestRetriever.RetrieveAsync(arguments.PullRequestNumber).GetAwaiter().GetResult();
 
