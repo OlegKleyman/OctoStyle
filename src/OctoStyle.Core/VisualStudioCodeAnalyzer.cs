9885de7f67658e98e6f3592e433fab646288ca80
Oleg Kleyman <Omego2K@gmail.com>
2015-09-06 11:36:54 -0400

    complete integration test for Roslyn analyzer

--- src/OctoStyle.Core/VisualStudioCodeAnalyzer.cs
+++ src/OctoStyle.Core/VisualStudioCodeAnalyzer.cs
@@ -2,6 +2,15 @@ namespace OctoStyle.Core
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Globalization;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using Microsoft.CodeAnalysis.MSBuild;
 
     public class VisualStudioCodeAnalyzer : ICodeAnalyzer
     {
@@ -24,7 +33,71 @@ namespace OctoStyle.Core
 
         public IEnumerable<GitHubStyleViolation> Analyze(string filePath)
         {
-            throw new System.NotImplementedException();
+            var workspace = MSBuildWorkspace.Create();
+
+            var solution = workspace.OpenSolutionAsync(solutionFilePath).GetAwaiter().GetResult();
+
+            var projectDiagnosticTasks = new List<Task<ImmutableArray<Diagnostic>>>();
+
+            foreach (var project in solution.Projects)
+            {
+                if (project.Language != LanguageNames.CSharp)
+                {
+                    continue;
+                }
+
+                projectDiagnosticTasks.Add(GetProjectAnalyzerDiagnostics(GetAllAnalyzers(), project));
+            }
+
+            var diagnosticBuilder = ImmutableList.CreateBuilder<Diagnostic>();
+
+            foreach (var task in projectDiagnosticTasks)
+            {
+                diagnosticBuilder.AddRange(task.ConfigureAwait(false).GetAwaiter().GetResult());
+            }
+
+            var violations =
+                diagnosticBuilder.Where(
+                    diagnostic =>
+                    string.Compare(diagnostic.Location.SourceTree.FilePath, filePath, StringComparison.OrdinalIgnoreCase) == 0)
+                    .Select(
+                        diagnostic =>
+                        new GitHubStyleViolation(
+                            diagnostic.Id,
+                            diagnostic.Descriptor.Description.ToString(CultureInfo.InvariantCulture),
+                            diagnostic.Location.GetLineSpan().EndLinePosition.Line));
+            
+            return violations;
+        }
+
+        private static ImmutableArray<DiagnosticAnalyzer> GetAllAnalyzers()
+        {
+            Assembly assembly = typeof(StyleCop.Analyzers.NoCodeFixAttribute).Assembly;
+
+            var diagnosticAnalyzerType = typeof(DiagnosticAnalyzer);
+
+            List<DiagnosticAnalyzer> analyzers = new List<DiagnosticAnalyzer>();
+
+            foreach (var type in assembly.GetTypes())
+            {
+                if (type.IsSubclassOf(diagnosticAnalyzerType) && !type.IsAbstract)
+                {
+                    analyzers.Add((DiagnosticAnalyzer)Activator.CreateInstance(type));
+                }
+            }
+
+            return analyzers.ToImmutableArray();
+        }
+
+        private static async Task<ImmutableArray<Diagnostic>> GetProjectAnalyzerDiagnostics(ImmutableArray<DiagnosticAnalyzer> analyzers, Project project)
+        {
+            Compilation compilation = await project.GetCompilationAsync().ConfigureAwait(false);
+            CompilationWithAnalyzers compilationWithAnalyzers = compilation.WithAnalyzers(analyzers);
+
+            var allDiagnostics = await compilationWithAnalyzers.GetAllDiagnosticsAsync().ConfigureAwait(false);
+
+            // We want analyzer diagnostics and analyzer exceptions
+            return allDiagnostics.RemoveRange(compilation.GetDiagnostics());
         }
     }
 }
--- tests/integration/OctoStyle.Core.Tests.Integration/VisualStudioCodeAnalyzerTests.cs
+++ tests/integration/OctoStyle.Core.Tests.Integration/VisualStudioCodeAnalyzerTests.cs
@@ -1,9 +1,93 @@
 ﻿namespace OctoStyle.Core.Tests.Integration
 {
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+
     using NUnit.Framework;
 
     [TestFixture]
     public class VisualStudioCodeAnalyzerTests
     {
+        [Test]
+        public void AnalyzeShouldReturnAllAnalysisIssues()
+        {
+            const string relativeSolutionPath = @"..\..\..\OctoStyleTest";
+            var projectPath = Path.GetFullPath(relativeSolutionPath);
+
+            if (projectPath == null)
+            {
+                throw new InvalidOperationException(
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        "Unable to retrieve project directory off relative path: {0}",
+                        relativeSolutionPath));
+            }
+
+            var analyzer = new VisualStudioCodeAnalyzer(Path.Combine(projectPath, "OctoStyleTest.sln"));
+
+            var violations =
+                analyzer.Analyze(Path.Combine(projectPath, @"src\TestLibrary\TestClass.cs"))
+                    .OrderBy(violation => violation.LineNumber).ThenBy(violation => violation.RuleId).ToList();
+            
+            Assert.That(violations.Count, Is.EqualTo(14));
+
+            Assert.That(violations[0].Message, Is.EqualTo("A single-line comment within a C# code file does not begin with a single space."));
+            Assert.That(violations[0].RuleId, Is.EqualTo("SA1005"));
+            Assert.That(violations[0].LineNumber, Is.EqualTo(0));
+
+            Assert.That(violations[1].Message, Is.EqualTo("A C# code file is missing a standard file header."));
+            Assert.That(violations[1].RuleId, Is.EqualTo("SA1633"));
+            Assert.That(violations[1].LineNumber, Is.EqualTo(0));
+            
+            Assert.That(violations[2].Message, Is.EqualTo("A C# using directive is placed outside of a namespace element."));
+            Assert.That(violations[2].RuleId, Is.EqualTo("SA1200"));
+            Assert.That(violations[2].LineNumber, Is.EqualTo(1));
+
+            Assert.That(violations[3].Message, Is.EqualTo("A C# using directive is placed outside of a namespace element."));
+            Assert.That(violations[3].RuleId, Is.EqualTo("SA1200"));
+            Assert.That(violations[3].LineNumber, Is.EqualTo(2));
+
+            Assert.That(violations[4].Message, Is.EqualTo("A C# using directive is placed outside of a namespace element."));
+            Assert.That(violations[4].RuleId, Is.EqualTo("SA1200"));
+            Assert.That(violations[4].LineNumber, Is.EqualTo(3));
+
+            Assert.That(violations[5].Message, Is.EqualTo("A C# using directive is placed outside of a namespace element."));
+            Assert.That(violations[5].RuleId, Is.EqualTo("SA1200"));
+            Assert.That(violations[5].LineNumber, Is.EqualTo(4));
+
+            Assert.That(violations[6].Message, Is.EqualTo("A C# using directive is placed outside of a namespace element."));
+            Assert.That(violations[6].RuleId, Is.EqualTo("SA1200"));
+            Assert.That(violations[6].LineNumber, Is.EqualTo(5));
+
+            Assert.That(violations[7].Message, Is.EqualTo("A C# code element is missing a documentation header."));
+            Assert.That(violations[7].RuleId, Is.EqualTo("SA1600"));
+            Assert.That(violations[7].LineNumber, Is.EqualTo(9));
+
+            Assert.That(violations[8].Message, Is.EqualTo("A C# code element is missing a documentation header."));
+            Assert.That(violations[8].RuleId, Is.EqualTo("SA1600"));
+            Assert.That(violations[8].LineNumber, Is.EqualTo(11));
+
+            Assert.That(violations[9].Message, Is.EqualTo("An opening curly bracket within a C# element, statement, or expression is followed by a blank line."));
+            Assert.That(violations[9].RuleId, Is.EqualTo("SA1505"));
+            Assert.That(violations[9].LineNumber, Is.EqualTo(14));
+
+            Assert.That(violations[10].Message, Is.EqualTo("A closing curly bracket within a C# element, statement, or expression is preceded by a blank line."));
+            Assert.That(violations[10].RuleId, Is.EqualTo("SA1508"));
+            Assert.That(violations[10].LineNumber, Is.EqualTo(16));
+
+            Assert.That(violations[11].Message, Is.EqualTo("A closing curly bracket within a C# element, statement, or expression is not followed by a blank line."));
+            Assert.That(violations[11].RuleId, Is.EqualTo("SA1513"));
+            Assert.That(violations[11].LineNumber, Is.EqualTo(17));
+
+            Assert.That(violations[12].Message, Is.EqualTo("A C# code element is missing a documentation header."));
+            Assert.That(violations[12].RuleId, Is.EqualTo("SA1600"));
+            Assert.That(violations[12].LineNumber, Is.EqualTo(20));
+
+            Assert.That(violations[13].Message, Is.EqualTo("A closing curly bracket within a C# element, statement, or expression is not followed by a blank line."));
+            Assert.That(violations[13].RuleId, Is.EqualTo("SA1513"));
+            Assert.That(violations[13].LineNumber, Is.EqualTo(25));
+        }
     }
 }
